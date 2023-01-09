b1c5d1e246db45a1c65f41aaac750796f84b40c9
HBEngineer2021 <stze961q@gmail.com>
2023-01-09 13:27:19 +0900

    Revert "① Add progress bar ."
    This reverts commit f45754bd161a15d9fbd251ac0f2b44a8f44beee0.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,11 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Header from '../components/Header';
 import { Footer } from '../components/Footer';
-import NextNProgress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Header />
-      <NextNProgress  color="#6366f1" />
       <Component {...pageProps} />
       <Footer />
     </>
