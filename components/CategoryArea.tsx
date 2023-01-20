56a099323cf64c8485e18461bee9c934745cd804
HBEngineer2021 <stze961q@gmail.com>
2023-01-21 00:05:31 +0900

    rename SideMenu ⇒ CategoryArea

--- components/CategoryArea.tsx
+++ components/CategoryArea.tsx
@@ -4,16 +4,16 @@ type Props = {
     categories: Array<Category>;
 };
 
-export function SideMenu({
+export function CategoryArea({
     categories,
 }: Props) {
     return (
-        <div className="pt-10 invisible md:visible lg:visible xl:visible">
+        <div className="container my-16 inline-block mx-auto invisible h-0 lg:visible xl:visible">
             <div className="container mx-auto bg-zinc-700 rounded">
                 <div className="container pl-6 pt-6 text-2xl font-semibold text-white">
                     #Category
                 </div>
-                <div className="container p-6 grid grid-cols-2 gap-2">
+                <div className="container p-6 grid grid-cols-1 xl:grid-cols-2 gap-2">
                     {categories.map(category => (
                         <div className="p-2" key={category.id}>
                             <span className="inline-block bg-zinc-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,10 @@ import type { InferGetStaticPropsType, NextPage } from 'next';
 import type { Blog } from '../types/article';
 import Link from 'next/link';
 import Head from 'next/head';
-import { SideMenu } from '../components/SideMenu';
+import { CategoryArea } from '../components/CategoryArea';
 import { Category } from '../types/category';
 import { PageControl } from '../components/PageControl';
+import { ProfileArea } from '../components/ProfileArea';
 
 type Props = {
   blogs: Array<Blog>;
@@ -26,31 +27,38 @@ const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
       <h1 className="container text-white font-bold text-lg mx-auto px-10 pt-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3">
         記事一覧
       </h1>
-      <div className="container mx-auto grid md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4">
+      <div className="container mx-auto grid md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4">
         <div className="container mx-auto p-10 grid grid-cols-1 col-span-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-5">
           {blogs.map(blog => (
-            <div className="shadow-lg p-3 mb-5 bg-zinc-700 rounded" key={blog.id}>
-              <img
-                className="w-full"
-                src={blog.eyecatch.url}
-                alt="Sunset in the mountains"
-              />
-              <div className="transition ease-in-out delay-150 text-white hover:text-zinc-600 duration-30 px-6 py-4 font-bold text-lg">
+            <div className="shadow-lg p-3 mb-5 bg-zinc-700 rounded transition ease-in-out delay-150 hover:-translate-y-3 hover:bg-zinc-500 duration-200" key={blog.id}>
+              <span>
                 <Link href={`/article/${blog.id}/`} passHref>
-                  <a>{blog.title}</a>
+                  <a>
+                    <img
+                      className="w-full"
+                      src={blog.eyecatch.url}
+                      alt="Sunset in the mountains"
+                    />
+                    <div className="transition ease-in-out delay-150 text-white hover:text-zinc-400 duration-30 px-6 py-4 font-bold text-lg">
+                      {blog.title}
+                    </div>
+                    <div className="px-6 pt-4 pb-2">
+                      {blog.category && (
+                        <span className="inline-block bg-zinc-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                          #{blog.category}
+                        </span>
+                      )}
+                    </div>
+                  </a>
                 </Link>
-              </div>
-              <div className="px-6 pt-4 pb-2">
-                {blog.category && (
-                  <span className="inline-block bg-zinc-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                    #{blog.category}
-                  </span>
-                )}
-              </div>
+              </span>
             </div>
           ))}
         </div>
-        <SideMenu categories={categories} />
+        <div>
+          <ProfileArea />
+          <CategoryArea categories={categories} />
+        </div>
       </div>
       {/* <PageControl blogs={blogs} /> */}
     </div>
@@ -58,7 +66,7 @@ const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
 }
 
 export const getStaticProps = async () => {
-  const blog = await client.get({ endpoint: "blogs", queries: {limit: 100} });
+  const blog = await client.get({ endpoint: "blogs", queries: { limit: 100 } });
   const category = await client.get({ endpoint: "categories" });
   return {
     props: {
