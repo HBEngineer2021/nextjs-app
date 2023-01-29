4d8039e14fff9ae427688ea2c596be8fd6bb4a68
HBEngineer2021 <stze961q@gmail.com>
2023-01-30 00:31:04 +0900

    fix layouts footer position and profile area, category area

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,8 @@ import { CategoryArea } from '../components/CategoryArea';
 import { Category } from '../types/category';
 import { PageControl } from '../components/PageControl';
 import { ProfileArea } from '../components/ProfileArea';
+import { Footer } from '../components/Footer';
+import { Grid } from '@mui/material';
 
 type Props = {
   blogs: Array<Blog>;
@@ -18,51 +20,57 @@ const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
   categories,
 }: Props) => {
   return (
-    <div className="w-full h-full">
+    <>
       <Head>
         <title>記事一覧 | Mobile Developer Docs</title>
         <meta name="description" content="iOS & Android Developer Documentation" />
         <meta name="theme-color" content="#27272A" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="container text-white font-bold text-lg mx-auto px-10 pt-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3">
+      <h1 className="container flex flex-wrap text-white font-bold text-lg mx-auto px-10 pt-10">
         記事一覧
       </h1>
-      <div className="container mx-auto grid md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4">
-        <div className="container mx-auto p-10 grid grid-cols-1 col-span-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-5">
-          {blogs.map(blog => (
-            <div className="shadow-lg p-3 mb-5 bg-zinc-700 rounded transition ease-in-out delay-150 hover:-translate-y-3 hover:bg-zinc-500 duration-200" key={blog.id}>
-              <span>
-                <Link href={`/article/${blog.id}/`} passHref>
-                  <a>
-                    <img
-                      className="w-full"
-                      src={blog.eyecatch.url}
-                      alt="Sunset in the mountains"
-                    />
-                    <div className="transition ease-in-out delay-150 text-white hover:text-zinc-400 duration-30 px-1 py-3 font-bold text-lg">
-                      {blog.title}
-                    </div>
-                    <div className="px-1 py-3">
-                      {blog.category && (
-                        <span className="inline-block bg-zinc-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                          #{blog.category}
-                        </span>
-                      )}
-                    </div>
-                  </a>
-                </Link>
-              </span>
+      <div className="flex justify-center">
+        <div className="container flex justify-start">
+          <div className="min-h-screen">
+            <div className="container flex flex-wrap mx-auto">
+              <div className="container mx-auto p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-4">
+                {blogs.map(blog => (
+                  <div className="shadow-lg p-3 mb-5 bg-zinc-700 rounded transition ease-in-out delay-150 hover:-translate-y-3 hover:bg-zinc-500 duration-200" key={blog.id}>
+                    <span>
+                      <Link href={`/article/${blog.id}/`} passHref>
+                        <a>
+                          <img
+                            className="w-full"
+                            src={blog.eyecatch.url}
+                            alt="Sunset in the mountains"
+                          />
+                          <div className="transition ease-in-out delay-150 text-white hover:text-zinc-400 duration-30 px-1 py-3 font-bold text-lg">
+                            {blog.title}
+                          </div>
+                          <div className="px-1 py-3">
+                            {blog.category && (
+                              <span className="inline-block bg-zinc-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                                #{blog.category}
+                              </span>
+                            )}
+                          </div>
+                        </a>
+                      </Link>
+                    </span>
+                  </div>
+                ))}
+                {/* <PageControl blogs={blogs} /> */}
+              </div>
             </div>
-          ))}
-        </div>
-        <div>
-          <ProfileArea />
-          <CategoryArea categories={categories} />
+          </div>
+          <div className="container inline-block invisible lg:visible xl:visible w-0 lg:w-1/3 xl:w-1/3">
+            <ProfileArea />
+            <CategoryArea categories={categories} />
+          </div>
         </div>
       </div>
-      {/* <PageControl blogs={blogs} /> */}
-    </div>
+    </>
   );
 }
 
